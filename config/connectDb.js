4db44162fd4ff3ea055a65a93f340b74ec2a4cb0
Cherry Mg Mg <cherry@kali.Min>
2023-12-30 10:21:16 -0500

    first Initialization

---
+++ config/connectDb.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const colors = require("colors");
+
+const connectDb = async () => {
+  try {
+
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("mongodb connected")
+    
+
+  } catch (error) {
+    console.log("connection fail")
+    console.log(`${error}`.bgRed);
+  }
+};
+
+module.exports = connectDb;
